505dd8008a8e5932c98790fa6541f58fdb59d25d
samurai <lastsamurai24@icloud.com>
2023-07-27 12:30:59 +0900

    Initial Commit

---
+++ matubatsuApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  matubatsuApp
+//
+//  Created by 待寺翼 on 2023/07/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
